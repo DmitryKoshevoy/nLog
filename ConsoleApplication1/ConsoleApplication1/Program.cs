6379d24979a47f13ace1e24aa80fb043c006c16c
DmitryKoshevoy <DmitryKoshevoy@github.com>
2014-12-08 18:06:56 +0300

    Init commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Array
+    {
+        public Array(Int32 lenth)
+        {
+            Output(lenth);
+        }
+
+        private Int32[] OutputArray(Int32 lenth)
+        {
+            Int32[] sample = new Int32[lenth];
+            for (var i = 0; i < sample.Length; i++)
+            {
+                sample[i] = i*2;
+            }
+            return sample;
+        }
+        private String OutputText(Int32 number, Int32 value)
+        {
+            return "sample[" + number + "]: " + value;
+        }
+
+        private void Output(Int32 lenth)
+        {
+            for (var i = 0; i < OutputArray(lenth).Length; i++)
+            {
+                Console.WriteLine(OutputText(i, OutputArray(lenth)[i]));
+            }
+        }
+    }
+    internal class Program
+    {
+        private static void Main()
+        {
+            var test = new Array(10);
+            Console.ReadKey();
+        }
+    }
+}
+
